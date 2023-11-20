2b2e70156404006b7cce03117d53ff729cc05b20
Ben Timby <btimby@gmail.com>
2023-11-20 18:35:06 -0500

    Reload extensions after rollup.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@ export default {
 
   data() {
     return {
-      msg: 'Hello Stegman!',
+      msg: 'Hello Aaron!',
     };
   },
 };
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import copy from '@guanghechen/rollup-plugin-copy';
+import execute from 'rollup-plugin-execute';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -22,24 +23,28 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      input: {
-        'index.html': path.resolve(__dirname, "index.html"),
+/*      input: {
+        main: path.resolve(__dirname, "index.html"),
         'src/content/index.js': path.resolve(__dirname, "src/content/index.js"),
         'background/index.html': path.resolve(__dirname, "src/background/index.html"),
       },
       output: {
         entryFileNames: '[name]',
-      },
+      },*/
       plugins: [
         copy({
           targets: [
             { src: 'manifest.json', dest: 'dist/'  },
             { src: 'manifestv3.json', dest: 'dist/'  },
             { src: 'src/content/index.css', dest: 'dist/src/content/' },
+            { src: 'src/content/index.js', dest: 'dist/src/content/' },
+            { src: 'src/background/index.html', dest: 'dist/src/background/' },
+            { src: 'src/background/index.js', dest: 'dist/src/background/' },
           ],
           verbose: true,
           hook: 'writeBundle',
-        })
+        }),
+        execute('xdg-open http://reload.extensions'),
       ]
     }
   }
