68da7b998dd01c751ee1a258259a10fc3fb771b9
Ben Timby <btimby@gmail.com>
2023-11-21 11:54:21 -0500

    Cleanup, starting over with webpack.

--- src/background/index.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.onMessage.addListener((msg, sender) => {
-    if ((msg.from === 'content') && msg.subject === 'openPageAction') {
-        chrome.pageAction.show(sender.tab.id);
-    }
-});
